e7a933f9e7d3f3bf02f13c6034497fcc1abdc990
Vitoria Tereza <vitoriatereza@MacBook-Air-de-Vitoria.local>
2025-08-30 01:26:32 -0300

    1-correcao-erros-compilacao

--- Program.cs
+++ Program.cs
@@ -12,14 +12,14 @@ namespace TransacaoFinanceira
 
         static void Main(string[] args)
         {
-            var TRANSACOES = new[] { new {correlation_id= 1,datetime="09/09/2023 14:15:00", conta_origem= 938485762, conta_destino= 2147483649, VALOR= 150},
-                                     new {correlation_id= 2,datetime="09/09/2023 14:15:05", conta_origem= 2147483649, conta_destino= 210385733, VALOR= 149},
-                                     new {correlation_id= 3,datetime="09/09/2023 14:15:29", conta_origem= 347586970, conta_destino= 238596054, VALOR= 1100},
-                                     new {correlation_id= 4,datetime="09/09/2023 14:17:00", conta_origem= 675869708, conta_destino= 210385733, VALOR= 5300},
-                                     new {correlation_id= 5,datetime="09/09/2023 14:18:00", conta_origem= 238596054, conta_destino= 674038564, VALOR= 1489},
-                                     new {correlation_id= 6,datetime="09/09/2023 14:18:20", conta_origem= 573659065, conta_destino= 563856300, VALOR= 49},
-                                     new {correlation_id= 7,datetime="09/09/2023 14:19:00", conta_origem= 938485762, conta_destino= 2147483649, VALOR= 44},
-                                     new {correlation_id= 8,datetime="09/09/2023 14:19:01", conta_origem= 573659065, conta_destino= 675869708, VALOR= 150},
+            var TRANSACOES = new[] { new {correlation_id= 1,datetime="09/09/2023 14:15:00", conta_origem= "938485762", conta_destino= "2147483649", VALOR= 150},
+                                     new {correlation_id= 2,datetime="09/09/2023 14:15:05", conta_origem= "2147483649", conta_destino= "210385733", VALOR= 149},
+                                     new {correlation_id= 3,datetime="09/09/2023 14:15:29", conta_origem= "347586970", conta_destino= "238596054", VALOR= 1100},
+                                     new {correlation_id= 4,datetime="09/09/2023 14:17:00", conta_origem= "675869708", conta_destino= "210385733", VALOR= 5300},
+                                     new {correlation_id= 5,datetime="09/09/2023 14:18:00", conta_origem= "238596054", conta_destino= "674038564", VALOR= 1489},
+                                     new {correlation_id= 6,datetime="09/09/2023 14:18:20", conta_origem= "573659065", conta_destino= "563856300", VALOR= 49},
+                                     new {correlation_id= 7,datetime="09/09/2023 14:19:00", conta_origem= "938485762", conta_destino= "2147483649", VALOR= 44},
+                                     new {correlation_id= 8,datetime="09/09/2023 14:19:01", conta_origem= "573659065", conta_destino= "675869708", VALOR= 150},
 
             };
             executarTransacaoFinanceira executor = new executarTransacaoFinanceira();
@@ -33,7 +33,7 @@ namespace TransacaoFinanceira
 
     class executarTransacaoFinanceira: acessoDados
     {
-        public void transferir(int correlation_id, int conta_origem, int conta_destino, decimal valor)
+        public void transferir(int correlation_id, string conta_origem, string conta_destino, decimal valor)
         {
             contas_saldo conta_saldo_origem = getSaldo<contas_saldo>(conta_origem) ;
             if (conta_saldo_origem.saldo < valor)
@@ -52,12 +52,12 @@ namespace TransacaoFinanceira
     }
     class contas_saldo
     {
-        public contas_saldo(int conta, decimal valor)
+        public contas_saldo(string conta, decimal valor)
         {
             this.conta = conta;
             this.saldo = valor;
         }
-        public int conta { get; set; }
+        public string conta { get; set; }
         public decimal saldo { get; set; }
     }
     class acessoDados
@@ -67,22 +67,22 @@ namespace TransacaoFinanceira
         public acessoDados()
         {
             TABELA_SALDOS = new List<contas_saldo>();
-            TABELA_SALDOS.Add(new contas_saldo(938485762, 180));
-            TABELA_SALDOS.Add(new contas_saldo(347586970, 1200));
-            TABELA_SALDOS.Add(new contas_saldo(2147483649, 0));
-            TABELA_SALDOS.Add(new contas_saldo(675869708, 4900));
-            TABELA_SALDOS.Add(new contas_saldo(238596054, 478));
-            TABELA_SALDOS.Add(new contas_saldo(573659065, 787));
-            TABELA_SALDOS.Add(new contas_saldo(210385733, 10));
-            TABELA_SALDOS.Add(new contas_saldo(674038564, 400));
-            TABELA_SALDOS.Add(new contas_saldo(563856300, 1200));
+            TABELA_SALDOS.Add(new contas_saldo("938485762", 180));
+            TABELA_SALDOS.Add(new contas_saldo("347586970", 1200));
+            TABELA_SALDOS.Add(new contas_saldo("2147483649", 0));
+            TABELA_SALDOS.Add(new contas_saldo("675869708", 4900));
+            TABELA_SALDOS.Add(new contas_saldo("238596054", 478));
+            TABELA_SALDOS.Add(new contas_saldo("573659065", 787));
+            TABELA_SALDOS.Add(new contas_saldo("210385733", 10));
+            TABELA_SALDOS.Add(new contas_saldo("674038564", 400));
+            TABELA_SALDOS.Add(new contas_saldo("563856300", 1200));
 
 
             SALDOS = new Dictionary<int, decimal>();
             this.SALDOS.Add(938485762, 180);
            
         }
-        public T getSaldo<T>(int id)
+        public T getSaldo<T>(string id)
         {          
             return (T)Convert.ChangeType(TABELA_SALDOS.Find(x => x.conta == id), typeof(T));
         }
